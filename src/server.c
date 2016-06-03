e291d4ce082c1c477703cd9b5adbfcdc9df4daa7
Enrico Vianello <vianello94@gmail.com>
2016-06-03 12:46:41 +0200

    Added a client list implementation

--- src/server.c
+++ src/server.c
@@ -19,12 +19,17 @@
 #define CMDLEN 32	// maximum length of the server's commands
 #define DEFAULTALIAS "Anonymous"	// default alias for new clients
 #define PAYLEN 1024	// payload size of a single packet
+#define MAXCLIENTS 64 // maximum number of clients connected
 
 /* Possible contenents of the packet's "action" field */
 #define EXIT 0
 #define ALIAS 1
 #define WHISPER 2
 #define SEND 3
+
+/******************************
+ * STRUCURE DEFINITION
+ ******************************/
 
 /* Structure containing the informations regarding a single connection with a client */
 struct ClientInfo {
@@ -39,6 +44,109 @@ struct Packet {
     char alias[ALIASLEN]; // client's alias
     char payload[PAYLEN]; // payload
 };
+
+/******************************
+ * END OF STRUCTURE DEFINITION
+ ******************************/
+
+/******************************
+ * LINKED LIST DEFINITION
+ * Every node of the list contains informations about one client
+ ******************************/
+
+/* Node of the list */
+struct LLNODE {
+	struct ClientInfo client_info;
+	struct LLNODE *next;
+};
+
+/* List */
+struct LLIST {
+	struct LLNODE *head, *tail;
+	int size;
+};
+
+/* Compare two ClientInfo struct, return 0 if they have the same connection socket */
+int compare(struct ClientInfo *a, struct ClientInfo *b) {
+	return a->sockfd - b->sockfd;
+}
+
+/* Initialize an empty list */
+void list_init(struct LLIST *ll) {
+	ll->head = ll->tail = NULL;
+	ll->size = 0;
+}
+
+/* Insert an element to the list, return -1 if the list is full */
+int list_insert(struct LLIST *ll, struct ClientInfo *cl_info) {
+	if(ll->size == MAXCLIENTS) return -1; // check if the list is full
+	/* If the list is empty, create the node and make head and tail point to it */
+	if(ll->head == NULL) {
+		ll->head = (struct LLNODE *)malloc(sizeof(struct LLNODE));
+		ll->head->client_info = *cl_info;
+		ll->head->next = NULL;
+		ll->tail = ll->head;
+	}
+	/* If the list isn't empty, create a new node and make the tail point to it */
+	else {
+		ll->tail->next = (struct LLNODE *)malloc(sizeof(struct LLNODE));
+		ll->tail->next->client_info = *cl_info;
+		ll->tail->next->next = NULL;
+		ll->tail = ll->tail->next;
+	}
+	ll->size++;
+	return 0;
+}
+
+/* Delete a client node from the list, return -1 if the element isn't found */
+int list_delete(struct LLIST *ll, struct ClientInfo *cl_info) {
+	struct LLNODE *curr, *tmp;
+	if(ll->head == NULL) return -1; // check if the structure is empty
+	/* Handle the case where the element to remove is the first */
+	if(!compare(cl_info, &ll->head->client_info)) {
+		tmp = ll->head; // store the element in a temporary variable to free the memory
+		ll->head = ll->head->next;
+		/* If the list is empty after the deletion, handle it correctly */
+		if(ll->head == NULL) {
+			ll->tail = ll->head;
+		}
+		free(tmp);
+		ll->size--;
+		return 0;
+	}
+	/* Search for the element to remove through the list */
+	for(curr = ll->head; curr->next != NULL; curr = curr->next) {
+		if(!compare(cl_info, &curr->next->client_info)) {
+			tmp = curr->next; // store the element in a temporary variable to free the memory
+			/* Handle the case where the element to remove is the last */
+			if(tmp == ll->tail) {
+				ll->tail = curr;
+				curr->next = NULL;
+			} else {
+				curr->next = curr->next->next;
+			}
+			free(tmp);
+			ll->size--;
+			return 0;
+		}
+	}
+	return -1;
+}
+
+/* Print the clients in a readable format */
+void list_dump(struct LLIST *ll) {
+	struct LLNODE *curr;
+	struct ClientInfo *cl_info;
+	printf("Connection count: %d\n", ll->size);
+	for(curr = ll->head; curr != NULL; curr = curr->next) {
+		cl_info = &curr->client_info;
+		printf("[%d] %s\n", cl_info->sockfd, cl_info->alias);
+	}
+}
+
+/******************************
+ * END OF LINKED LIST
+ ******************************/
 
 /* FIELDS */
 static int sockfd;									// socket listening for incoming connections
