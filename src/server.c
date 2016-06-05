604622d70bd85e59aeb8299142e7e63fff2ce016
Enrico Vianello <vianello94@gmail.com>
2016-06-05 12:41:44 +0200

    Added packet handling

--- src/server.c
+++ src/server.c
@@ -24,11 +24,12 @@
 /* Possible contenents of the packet's "action" field */
 #define EXIT 0
 #define ALIAS 1
-#define WHISPER 2
-#define SEND 3
+#define MSG 2
+#define WHISPER 3
+#define SHOUT 4
 
 /******************************
- * STRUCURE DEFINITION
+* STRUCTURE DEFINITION
  ******************************/
 
 /* Structure containing the informations regarding a single connection with a client */
@@ -55,14 +56,14 @@ struct Packet {
  ******************************/
 
 /* Node of the list */
-struct LLNODE {
+struct LLNode {
 	struct ClientInfo client_info;
-	struct LLNODE *next;
+	struct LLNode *next;
 };
 
 /* List */
-struct LLIST {
-	struct LLNODE *head, *tail;
+struct LinkedList {
+	struct LLNode *head, *tail;
 	int size;
 };
 
@@ -72,24 +73,24 @@ int compare(struct ClientInfo *a, struct ClientInfo *b) {
 }
 
 /* Initialize an empty list */
-void list_init(struct LLIST *ll) {
+void list_init(struct LinkedList *ll) {
 	ll->head = ll->tail = NULL;
 	ll->size = 0;
 }
 
 /* Insert an element to the list, return -1 if the list is full */
-int list_insert(struct LLIST *ll, struct ClientInfo *cl_info) {
+int list_insert(struct LinkedList *ll, struct ClientInfo *cl_info) {
 	if(ll->size == MAXCLIENTS) return -1; // check if the list is full
 	/* If the list is empty, create the node and make head and tail point to it */
 	if(ll->head == NULL) {
-		ll->head = (struct LLNODE *)malloc(sizeof(struct LLNODE));
+		ll->head = (struct LLNode *)malloc(sizeof(struct LLNode));
 		ll->head->client_info = *cl_info;
 		ll->head->next = NULL;
 		ll->tail = ll->head;
 	}
 	/* If the list isn't empty, create a new node and make the tail point to it */
 	else {
-		ll->tail->next = (struct LLNODE *)malloc(sizeof(struct LLNODE));
+		ll->tail->next = (struct LLNode *)malloc(sizeof(struct LLNode));
 		ll->tail->next->client_info = *cl_info;
 		ll->tail->next->next = NULL;
 		ll->tail = ll->tail->next;
@@ -99,8 +100,8 @@ int list_insert(struct LLIST *ll, struct ClientInfo *cl_info) {
 }
 
 /* Delete a client node from the list, return -1 if the element isn't found */
-int list_delete(struct LLIST *ll, struct ClientInfo *cl_info) {
-	struct LLNODE *curr, *tmp;
+int list_delete(struct LinkedList *ll, struct ClientInfo *cl_info) {
+	struct LLNode *curr, *tmp;
 	if(ll->head == NULL) return -1; // check if the structure is empty
 	/* Handle the case where the element to remove is the first */
 	if(!compare(cl_info, &ll->head->client_info)) {
@@ -134,8 +135,8 @@ int list_delete(struct LLIST *ll, struct ClientInfo *cl_info) {
 }
 
 /* Print the clients in a readable format */
-void list_dump(struct LLIST *ll) {
-	struct LLNODE *curr;
+void list_dump(struct LinkedList *ll) {
+	struct LLNode *curr;
 	struct ClientInfo *cl_info;
 	printf("Connection count: %d\n", ll->size);
 	for(curr = ll->head; curr != NULL; curr = curr->next) {
@@ -149,10 +150,13 @@ void list_dump(struct LLIST *ll) {
  ******************************/
 
 /* FIELDS */
-static int sockfd;									// socket listening for incoming connections
-int connectfd;							// socket to perform actions
-struct addrinfo hints;					// structure used to set the preferencies for servinfo
-struct addrinfo *servinfo;				// structure containing local data
+static int sockfd;					// socket listening for incoming connections
+int connectfd;						// socket to perform actions
+struct addrinfo hints;				// structure used to set the preferencies for servinfo
+struct addrinfo *servinfo;			// structure containing local data
+struct LinkedList client_list;		// list of the connected clients
+pthread_mutex_t clientlist_mutex;	// mutual exclusion variable preventing concurrent
+									// edits to the client list
 
 // get sockaddr, correctly formatted: IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa) {
@@ -167,17 +171,18 @@ void *server_handler(void *param) {
 	char command[CMDLEN];
 	while(scanf("%s", command) == 1) {
 		if(!strcmp(command, "exit") || !strcmp(command, "quit")) {
-			/* clean up */
+			/* Clean up and terminate the program */
 			printf("Terminating server...\n");
-//			pthread_mutex_destroy(&clientlist_mutex);
+			pthread_mutex_destroy(&clientlist_mutex); // delete the mutex
 			close(sockfd); // close the listening socket
 			exit(0);
 		}
-//		else if(!strcmp(command, "list")) {
-//			pthread_mutex_lock(&clientlist_mutex);
-//			list_dump(&client_list);
-//			pthread_mutex_unlock(&clientlist_mutex);
-//		}
+		else if(!strcmp(command, "list")) {
+			/* Print a dump of the current client list */
+			pthread_mutex_lock(&clientlist_mutex);
+			list_dump(&client_list);
+			pthread_mutex_unlock(&clientlist_mutex);
+		}
 		else {
 			fprintf(stderr, "Unknown command: %s\n", command);
 		}
@@ -188,33 +193,110 @@ void *server_handler(void *param) {
 void *client_handler(void *info) {
 	struct ClientInfo client_info = *(struct ClientInfo *)info;
 	struct Packet packet;
-	struct LLNODE *curr;
-	int sent;
+	struct LLNode *curr;
 	printf("DEBUG: Thread started for the client named %s\n", client_info.alias);
 	while(1) {
 		/* Receive a packet of data from the client */
 		if(!recv(client_info.sockfd, (void *)&packet, sizeof(struct Packet), 0)) {
 			/* Connection with the client lost */
-			fprintf(stderr, "Connection lost from [%d] %s...\n", client_info.sockfd, client_info.alias);
-//			pthread_mutex_lock(&clientlist_mutex);
-//			list_delete(&client_list, &client_info); // remove the client from the linked list
-//			pthread_mutex_unlock(&clientlist_mutex);
+			fprintf(stderr, "Connection lost from [%d] %s\n", client_info.sockfd, client_info.alias);
+			/* Remove the client from the client list */
+			pthread_mutex_lock(&clientlist_mutex);
+			list_delete(&client_list, &client_info); // remove the client from the linked list
+			pthread_mutex_unlock(&clientlist_mutex);
 			break;
+			printf("DEBUG: This shouldn't be printed if a client thread is closed");
 		}
-		/* Print the entire contenent of the received packet */
-		printf("[%d] action_code:%d | %s | %s\n", client_info.sockfd, packet.action, packet.alias, packet.payload);
+		printf("Packet received:[%d] action_code=%d | %s | %s\n", client_info.sockfd, packet.action, packet.alias, packet.payload);
+		switch (packet.action) {
+			/* Change the client's alias */
+			case ALIAS :
+				printf("server: user #%d is changing his alias from '%s' to '%s'\n", client_info.sockfd, client_info.alias, packet.alias);
+				pthread_mutex_lock(&clientlist_mutex);
+				/* Search the client in the list and edit his alias */
+				for(curr = client_list.head; curr != NULL; curr = curr->next) {
+					if(compare(&curr->client_info, &client_info) == 0) {
+						strcpy(curr->client_info.alias, packet.alias);
+						strcpy(client_info.alias, packet.alias);
+					}
+				}
+				pthread_mutex_unlock(&clientlist_mutex);
+				break;
+			/* Send a message to a specific client */
+			case WHISPER : ; // empty statement necessary to compile
+				/* Acquire the target client */
+				int i;
+				char target[ALIASLEN];
+				for(i = 0; packet.payload[i] != ' '; i++);
+				packet.payload[i++] = '\0'; // replace the space after the target's alias with a termination
+				strcpy(target, packet.payload);
+				/* Find the target client and send the message */
+				pthread_mutex_lock(&clientlist_mutex);
+				for(curr = client_list.head; curr != NULL; curr = curr->next) {
+					if(strcmp(target, curr->client_info.alias) == 0) {
+						/* If the found client is the sender, keep searching */
+						if(!compare(&curr->client_info, &client_info)) {
+							continue;
+						}
+						/* Build a new packet only containing the message */
+						struct Packet msgpacket;
+						memset(&msgpacket, 0, sizeof(struct Packet));
+						msgpacket.action = MSG;
+						strcpy(msgpacket.alias, packet.alias);
+						strcpy(msgpacket.payload, &packet.payload[i]); // the payload of the new packet contains just the message
+						if (send(curr->client_info.sockfd, (void *)&msgpacket, sizeof(struct Packet), 0) == -1) {
+							perror("server: send");
+						}
+					}
+				}
+				pthread_mutex_unlock(&clientlist_mutex);
+				break;
+			/* Send a message to every client connected */
+			case SHOUT :
+				pthread_mutex_lock(&clientlist_mutex);
+				for(curr = client_list.head; curr != NULL; curr = curr->next) {
+					/* If the found client is the sender, keep searching */
+					if(!compare(&curr->client_info, &client_info)) {
+						continue;
+					}
+					/* Build a new packet containing the message and send it */
+					struct Packet msgpacket;
+					memset(&msgpacket, 0, sizeof(struct Packet));
+					msgpacket.action = MSG;
+					strcpy(msgpacket.alias, packet.alias);
+					strcpy(msgpacket.payload, packet.payload);
+					if (send(curr->client_info.sockfd, (void *)&msgpacket, sizeof(struct Packet), 0) == -1) {
+						perror("server: send");
+					}
+				}
+				pthread_mutex_unlock(&clientlist_mutex);
+				break;
+			/* Terminate the connection */
+			case EXIT :
+				printf("[%d] %s has disconnected\n", client_info.sockfd, client_info.alias);
+				pthread_mutex_lock(&clientlist_mutex);
+				list_delete(&client_list, &client_info);
+				pthread_mutex_unlock(&clientlist_mutex);
+				break;
+			default :
+				fprintf(stderr, "Unidentified packet from [%d] %s : action_code=%d\n", client_info.sockfd, client_info.alias, packet.action);
+		}
 	}
 
-	/*  */
+	/* Close the client socket */
 	close(client_info.sockfd);
 
 	return NULL;
 }
 
 int main(void) {
+	/* initialize client list */
+	list_init(&client_list);
+	/* initiate mutex */
+	pthread_mutex_init(&clientlist_mutex, NULL);
 
 	/******************************
-	 * Fill up local address
+	 * Set up the listener socket
 	 ******************************/
 
 	memset(&hints, 0, sizeof hints);	// make sure the struct is empty
@@ -228,12 +310,6 @@ int main(void) {
 		return -1;
 	}
 
-	printf("DEBUG: Local address ready\n");
-
-	/******************************
-	 * Set the listener
-	 ******************************/
-
 	// loop through all the elements returned by getaddrinfo and bind the first possible
 	struct addrinfo *p;	// pointer used to inspect servinfo
 	for(p = servinfo; p != NULL; p = p->ai_next) {
@@ -243,7 +319,7 @@ int main(void) {
 			printf("DEBUG: socket creation caused an error\n");
 			continue;	// in case of error with this address, try with another iteraction
 		}
-		// allow other socket to bind this port when not listening
+		// allow other sockets to bind this port when not listening
 		int yes = 1;
 		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
 			perror("server: setsockopt");
@@ -269,8 +345,6 @@ int main(void) {
 		return -1;
 	}
 
-	printf("DEBUG: socket successfully binded to a local address\n");
-
 	// start listening
 	if (listen(sockfd, BACKLOG) == -1) {
 		perror("server: listen");
@@ -290,7 +364,7 @@ int main(void) {
 		return -1;
 	}
 
-	struct sockaddr_storage client_addr; 	// connector's address information
+	struct sockaddr_storage client_addr;	// connector's address information
 	socklen_t sin_size;						// dimension of the connector's sockaddr structure
 	int new_fd = -1;						// temporary file descriptor for the incoming connections
 
@@ -310,17 +384,18 @@ int main(void) {
 		inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr *)&client_addr), s, sizeof s);
 		printf("server: got connection from %s\n", s);
 
-		/* set the necessary data and start a thread to handle the connection */
+		/* Set the client data */
 		struct  ClientInfo client_info;
 		client_info.sockfd = new_fd;
 		strcpy(client_info.alias, DEFAULTALIAS);
-//		pthread_mutex_lock(&clientlist_mutex);
-//		list_insert(&client_list, &client_info);
-//		pthread_mutex_unlock(&clientlist_mutex);
 
-		/* create a thread to handle the new client */
+		/* Add the new client to the client list */
+		pthread_mutex_lock(&clientlist_mutex);
+		list_insert(&client_list, &client_info);
+		pthread_mutex_unlock(&clientlist_mutex);
+
+		/* Create a thread to handle the new client */
 		pthread_create(&client_info.thread_ID, NULL, client_handler, (void *)&client_info);
-
 	}
 
 	return 0;
